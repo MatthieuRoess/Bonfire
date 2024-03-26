53b79963b44e2482d3793cb0b84eb5a6ff30b565
MatthieuRoess <123086572+MatthieuRoess@users.noreply.github.com>
2024-03-26 16:35:42 +0100

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connectDB() {
   try {
-    await mongoose.connect('mongodb+srv://tristan:R22dcbd1t29163uj@cluster0.yq5wjyn.mongodb.net/?retryWrites=true&w=majority',{dbName: 'Project_IRC'});
+    await mongoose.connect('YOUR_URI',{dbName: 'Project_IRC'});
   } catch (error) {
     console.error('Error connecting to MongoDB Atlas:', error);
   }
